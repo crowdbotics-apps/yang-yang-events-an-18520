a39fa6ab02facf19b7724736609c8c194f56c696
Crowdbotics <team@crowdbotics.com>
2020-06-30 22:38:42 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/yang_yang_photos_18520/MainActivity.java
@@ -0,0 +1,15 @@
+package com.yang_yang_photos_18520;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "yang_yang_photos_18520";
+    }
+}
